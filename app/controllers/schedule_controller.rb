e123a17ed0f42c5055d9ce863f259d4b48d646b1
hmanoj59 <hotasmail@gmail.com>
2016-07-21 19:02:38 -0700

    add skejuler UI, added model and controller

---
+++ app/controllers/schedule_controller.rb
@@ -0,0 +1,13 @@
+class ScheduleController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :dashboard, only: [:index]
+  resources :schedule, only: [:index, :new]
+
 
   root 'high_voltage/pages#show', id: 'about'
 end
