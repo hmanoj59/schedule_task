bdf842451c120c5017cbbd10812094de618a81c6
hmanoj59 <hotasmail@gmail.com>
2016-07-22 20:02:37 -0700

    Made changes in calling the scheduler form

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :dashboard, only: [:index]
-  resources :schedule, only: [:index, :new]
+  resources :schedules, only: [:index, :new]
 
 
   root 'high_voltage/pages#show', id: 'about'
